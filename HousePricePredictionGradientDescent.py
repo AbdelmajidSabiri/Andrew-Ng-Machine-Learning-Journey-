134c962f5575bafe2342e62f55475e35d6cb6685
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-09-28 16:22:47 +0100

    Add function to find better parameters w,b for our model

--- HousePricePredictionGradientDescent.py
+++ HousePricePredictionGradientDescent.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import copy,math
 
 X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 y_train = np.array([460, 232, 178])
@@ -8,19 +8,21 @@ b_init = 785.1811367994083
 w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
 
 
+def compute_cost(X, y, w, b): 
+
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):                                
+        f_wb_i = np.dot(X[i], w) + b       
+        cost = cost + (f_wb_i - y[i])**2    
+    cost = cost / (2 * m)                  
+    return cost
+
+
+
+# Function to calculate derivitive dj/dw, dj/db
 def compute_gradient(X, y, w, b): 
-    """
-    Computes the gradient for linear regression 
-    Args:
-      X (ndarray (m,n)): Data, m examples with n features
-      y (ndarray (m,)) : target values
-      w (ndarray (n,)) : model parameters  
-      b (scalar)       : model parameter
-      
-    Returns:
-      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
-      dj_db (scalar):       The gradient of the cost w.r.t. the parameter b. 
-    """
+
     m,n = X.shape           #(number of examples, number of features)
     dj_dw = np.zeros((n,))
     dj_db = 0.
@@ -34,3 +36,44 @@ def compute_gradient(X, y, w, b):
     dj_db = dj_db / m                                
         
     return dj_db, dj_dw
+
+
+
+def gradient_descent(X, y, w_in, b_in, cost_function, compute_gradient, alpha, num_iters): 
+
+    J_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db,dj_dw = compute_gradient(X, y, w, b)  
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw             
+        b = b - alpha * dj_db              
+      
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( cost_function(X, y, w, b))
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}   ")
+        
+    return w, b
+
+
+# initialize parameters
+initial_w = np.zeros_like(w_init)
+initial_b = 0.
+# some gradient descent settings
+iterations = 200000
+alpha = 5.0e-7
+# run gradient descent 
+w_final, b_final = gradient_descent(X_train, y_train, initial_w, initial_b,compute_cost, compute_gradient, alpha, iterations)
+print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
+m,_ = X_train.shape
+for i in range(m):
+    print(f"prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")
